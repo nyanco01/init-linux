260f7f187d8c409c4145642c586436e8954dfde9
nyanco01 <yuta.ta@icloud.com>
2022-10-30 05:25:46 +0000

    fix zsh.sh

--- ubuntu/app/zsh.sh
+++ ubuntu/app/zsh.sh
@@ -4,12 +4,12 @@ source $HOME/init-linux/resource/import.sh
 CHECK sudo apt install -y zsh
 [[ `echo $SHELL | grep 'zsh'` ]] || chsh -s /bin/zsh
 
-CHECK curl https://sh.rustup.rs -sSf | sh
+CHECK curl https://sh.rustup.rs -sSf | sh < yes 1
 CHECK source $HOME/.cargo/env
 CHECK cargo install powerline-rs
 CHECK sudo apt install -y pkg-config libssl-dev
 CHECK cargo install sheldon
-CHECK sheldon init --shell zsh
+CHECK yes | sheldon init --shell zsh
 
 if [ -f $HOME/.zshrc ];then
     CHECK rm $HOME/.zshrc
