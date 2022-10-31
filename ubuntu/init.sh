7e5b1b5500ad266340a153a8933d2cd5328de39b
nyanco01 <yuta.ta@icloud.com>
2022-10-31 05:38:16 +0000

    add needrestart in init.sh

--- ubuntu/init.sh
+++ ubuntu/init.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 source $HOME/init-linux/resource/import.sh
 
+CHECK echo "\$nrconf{restart} = 'a';" | sudo tee /etc/needrestart/conf.d/50local.conf
+
 CHECK sudo apt update
 CHECK sudo apt upgrade -y
 CHECK sudo apt install -y curl git build-essential
